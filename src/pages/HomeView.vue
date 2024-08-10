4339cfdfe3f0f49c33233a02bbd599a85a25dca7
kaikai <2431208434@qq.com>
2024-08-10 18:58:34 +0800

    feat: 完善首页内容1.0

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,7 +1,37 @@
 <template>
-  <div>我是主页</div>
+  <div class="body">
+    <div class="top"></div>
+    <div class="content">
+      <h1 class="title">About me</h1>
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.body {
+  .top {
+    height: 160px;
+    background-color: var(--themeColor);
+  }
+
+  .content {
+    position: relative;
+    height: 100px;
+    background-color: #fff;
+    border-radius: 10px;
+    margin: -66px 32px;
+
+    .title {
+      position: absolute;
+      left: 0;
+      top: -55px;
+      font-size: 34px;
+      font-weight: 600;
+      letter-spacing: 0.05em;
+      color: white;
+    }
+  }
+}
+</style>
 
 <script setup lang="ts"></script>
