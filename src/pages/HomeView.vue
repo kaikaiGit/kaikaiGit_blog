459d02dbc08651d8f8e9e6a9f2b1c6a51bcc31f5
kaikai <2431208434@qq.com>
2024-08-11 10:54:41 +0800

    feat: 引入字体库

--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -8,6 +8,14 @@
 </template>
 
 <style lang="scss" scoped>
+@font-face {
+  font-family: 'dd Regular';
+  src:
+    url('@/assets/fonts/dd_Regular/bjJcJl24BO6h.woff2') format('woff2'),
+    url('@/assets/fonts/dd_Regular/bjJcJl24BO6h.woff') format('woff');
+  font-display: swap;
+}
+
 .body {
   .top {
     height: 160px;
@@ -20,13 +28,15 @@
     background-color: #fff;
     border-radius: 10px;
     margin: -66px 32px;
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
 
     .title {
       position: absolute;
       left: 0;
       top: -55px;
       font-size: 34px;
-      font-weight: 600;
+      font-weight: 700;
+      font-family: 'dd Regular';
       letter-spacing: 0.05em;
       color: white;
     }
