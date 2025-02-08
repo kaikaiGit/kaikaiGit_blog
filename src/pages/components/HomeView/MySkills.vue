bb813f6b90462588ad76cd0902650624d996e40d
kaikai <2431208434@qq.com>
2025-02-08 18:09:49 +0800

    完善暗黑模式下的卡片阴影

--- src/pages/components/HomeView/MySkills.vue
+++ src/pages/components/HomeView/MySkills.vue
@@ -354,7 +354,7 @@ const courseData = [
 
 .card {
   border-radius: 14px;
-  box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
+  box-shadow: 0 2px 12px 0 var(--boxShadowColor);
   overflow: hidden;
 }
 
